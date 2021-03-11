799077dc8a50fa0880b5c6b0d6a64f65cfe5b53e
bogdan pryvalov <bohdan.privalov@tech-stack.io>
2021-03-11 23:42:13 +0200

    added for start eslint config

---
+++ src/modules/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React, { memo } from 'react';
+import { Wrapper, GlobalStyles, Title } from './styledComponents';
+
+export const Layout = memo((props) => {
+  const some = props?.sone;
+  return (
+    <Wrapper>
+      <Title>Layout</Title>
+      {some}
+      <GlobalStyles />
+    </Wrapper>
+  );
+});
